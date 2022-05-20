0ae99f6eb086b695e77831d2be29bca58bfdf91c
Mark Warriner <mark.warriner@madetech.com>
2022-05-20 11:20:10 +0100

    remove user role - not required for IAM

--- datalake/13-emr-studio.tf
+++ datalake/13-emr-studio.tf
@@ -5,7 +5,6 @@ resource "aws_emr_studio" "emr_studio" {
   name                        = "emr_studio"
   service_role                = aws_iam_role.iam_emr_studio_role.arn
   subnet_ids                  = [aws_subnet.main.id]
-  user_role                   = aws_iam_role.iam_emr_service_role.arn
   vpc_id                      = aws_vpc.main.id
   workspace_security_group_id = aws_security_group.allow_access.id
 }
