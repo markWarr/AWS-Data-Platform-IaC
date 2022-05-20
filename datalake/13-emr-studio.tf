c0596f0390936e7f787e96b01d81caa9f1e54db8
Mark Warriner <mark.warriner@madetech.com>
2022-05-20 11:18:43 +0100

    Change emr studio auth mode to IAM

--- datalake/13-emr-studio.tf
+++ datalake/13-emr-studio.tf
@@ -1,5 +1,5 @@
 resource "aws_emr_studio" "emr_studio" {
-  auth_mode                   = "SSO"
+  auth_mode                   = "IAM"
   default_s3_location         = "s3://${var.s3_emr_studio_name}"
   engine_security_group_id    = aws_security_group.allow_access.id
   name                        = "emr_studio"
